14464431deaf358f34c83a0497a838bb1615a86e
Brian Hartmann <brianhartmann@Brians-MacBook-Air.local>
2022-10-25 13:46:11 -0400

    clean up code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      fontFamily: {
+        'knot-font': 'open-sans-condensed'
+      },
+      backgroundImage: {
+      'texture': "url('https://media-api.xogrp.com/images/8d675785-1659-4546-8760-4cd6ec34117c~rt_auto-rs_1600.h?ordering=explicit')",
+      },
+      colors: {
+        'regal-blue': '#243c5a',
+        'knot-background': '#f8f8f8',
+        'knot-textcolor': '#666A6B'
+      },
+    },
   },
   plugins: [],
-}
+};
