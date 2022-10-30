8ae3a5fb10aff6da27eeca0c6edad687a5f8abaf
Brian Hartmann <brianhartmann@Brians-MacBook-Air.local>
2022-10-30 17:15:20 -0400

    finish first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'knot-font': 'open-sans-condensed'
+        'ffont': 'pacifico',
+        'knot-font': 'open-sans-condensed',
+        'cursive': 'name-font'
       },
       backgroundImage: {
       'texture': "url('https://media-api.xogrp.com/images/8d675785-1659-4546-8760-4cd6ec34117c~rt_auto-rs_1600.h?ordering=explicit')",
